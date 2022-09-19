344a093c0d9ccbc69ff41386e20c94ed9efc020e
vortydev <etienne.dmenard@gmail.com>
2022-09-19 15:25:25 -0400

    tables account, role et state

--- server/src/persistence/mysql.js
+++ server/src/persistence/mysql.js
@@ -39,56 +39,48 @@ async function init() {
             'CREATE TABLE IF NOT EXISTS todo_items (id varchar(36), name varchar(255), completed boolean) DEFAULT CHARSET utf8mb4',
             err => {
                 if (err) return rej(err);
-
                 console.log(`Connected to mysql db at host ${HOST}`);
                 acc();
             },
         );
 
-        // create table rôle utilisateur
+        /* UTILISATEURS */
+
+        // tbl_role_account
         pool.query(
-            'CREATE TABLE IF NOT EXISTS tbl_role (id int(8) NOT NULL AUTO_INCREMENT, role varchar(32) NOT NULL, CONSTRAINT PK_Role PRIMARY KEY (`id`)) DEFAULT CHARSET utf8mb4',
+            'CREATE TABLE IF NOT EXISTS tbl_role_account (role_id int(8) NOT NULL AUTO_INCREMENT, role_name varchar(255) NOT NULL, CONSTRAINT PK_RoleAccount PRIMARY KEY (role_id)) DEFAULT CHARSET utf8mb4',
             err => {
                 if (err) return rej(err);
                 acc();
             },
         );
 
-        // create table statut utilisateur
+        // tbl_state_account
         pool.query(
-            'CREATE TABLE IF NOT EXISTS tbl_statut (id int(8) NOT NULL AUTO_INCREMENT, statut varchar(32) NOT NULL, CONSTRAINT PK_Statut PRIMARY KEY (`id`)) DEFAULT CHARSET utf8mb4',
+            'CREATE TABLE IF NOT EXISTS tbl_state_account (state_id int(8) NOT NULL AUTO_INCREMENT, state_name varchar(255) NOT NULL, CONSTRAINT PK_StateAccount PRIMARY KEY (state_id)) DEFAULT CHARSET utf8mb4',
             err => {
                 if (err) return rej(err);
                 acc();
             },
         );
 
-        // insert statuts utilisateur
-        // pool.query(
-        //     'INSERT INTO tbl_statut (id, statut) VALUES (1, `Inactif`), (2, `Actif`), (3, `Désactivté`);',
-        //     err => {
-        //         if (err) return rej(err);
-        //         acc();
-        //     },
-        // );
-
-        // insert roles utilisateur
-        // pool.query(
-        //     'INSERT INTO tbl_role (id, role) VALUES (1, `Admin`), (2, `Employé`), (3, `Livreur`);',
-        //     err => {
-        //         if (err) return rej(err);
-        //         acc();
-        //     },
-        // );
+        // tbl_account
+        pool.query(
+            'CREATE TABLE IF NOT EXISTS tbl_account (acc_id int(8) NOT NULL AUTO_INCREMENT, acc_name varchar(255) NOT NULL, acc_role_id int(8) NOT NULL, acc_state_id int(8) NOT NULL, acc_date_creation DATETIME, FOREIGN KEY (acc_role_id) REFERENCES tbl_role_account(role_id), FOREIGN KEY (acc_state_id) REFERENCES tbl_state_account(state_id), CONSTRAINT PK_Account PRIMARY KEY (acc_id)) DEFAULT CHARSET utf8mb4',
+            err => {
+                if (err) return rej(err);
+                acc();
+            },
+        );
 
         // create table utilisateur
-        pool.query(
-            'CREATE TABLE IF NOT EXISTS tbl_utilisateur (id int(8) NOT NULL AUTO_INCREMENT, username varchar(255) NOT NULL, email varchar(255) NOT NULL, mdp varchar(255) NOT NULL, idRole int(8) NOT NULL, idStatut int(8) NOT NULL, dateCreation DATETIME NOT NULL, CONSTRAINT PK_Utilisateur PRIMARY KEY (`id`), CONSTRAINT FK_UserRole FOREIGN KEY (`idRole`) REFERENCES `tbl_role` (`id`), CONSTRAINT FK_UserStatut FOREIGN KEY (`idStatut`) REFERENCES `tbl_statut` (`id`)) DEFAULT CHARSET utf8mb4',
-            err => {
-                if (err) return rej(err);
-                acc();
-            },
-        );
+        // pool.query(
+        //     'CREATE TABLE IF NOT EXISTS tbl_utilisateur (id int(8) NOT NULL AUTO_INCREMENT, username varchar(255) NOT NULL, email varchar(255) NOT NULL, mdp varchar(255) NOT NULL, idRole int(8) NOT NULL, idStatut int(8) NOT NULL, dateCreation DATETIME NOT NULL, CONSTRAINT PK_Utilisateur PRIMARY KEY (`id`), CONSTRAINT FK_UserRole FOREIGN KEY (`idRole`) REFERENCES `tbl_role` (`id`), CONSTRAINT FK_UserStatut FOREIGN KEY (`idStatut`) REFERENCES `tbl_statut` (`id`)) DEFAULT CHARSET utf8mb4',
+        //     err => {
+        //         if (err) return rej(err);
+        //         acc();
+        //     },
+        // );
     });
 }
 

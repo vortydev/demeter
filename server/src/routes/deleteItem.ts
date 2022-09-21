1833ee5ed3cb353657ea0e35ce36304bd4079c71
devweb <devweb.chronomed@gmail.com>
2022-09-21 13:03:17 -0400

    New architecture more centered around components

--- server/src/routes/deleteItem.ts
+++
@@ -1,9 +0,0 @@
-import db from '../persistence';
-
-const deleteItem = async (req:any, res:any) => {
-    await db.removeItem(req.params.id);
-    res.sendStatus(200);
-};
-
-
-export {deleteItem}
--- server/src/routes/getItems.ts
+++
@@ -1,8 +0,0 @@
-import db from '../persistence';
-
-const getItems = async (req:any, res:any) => {
-    const items = await db.getItems();
-    res.send(items);
-};
-
-export {getItems};

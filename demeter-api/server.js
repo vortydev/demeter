875f2c0ff57a630e0dde54d9f25ce4b18c6d97eb
vortydev <etienne.dmenard@gmail.com>
2022-10-07 09:07:57 -0400

    api and ui work, cant connect to db

---
+++ demeter-api/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+var corsOptions = {
+  origin: process.env.CLIENT_ORIGIN || "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// load database
+const db = require("./app/models");
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+// drop existing tables and resync
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Partie backend de l'application." });
+});
+
+// routes
+require("./app/routes/tutorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
---
+++ demeter-ui/src/types/tutorial.type.ts
@@ -0,0 +1,7 @@
+export default interface ITutorialData {
+    id?: any | null,
+    title: string,
+    description: string,
+    published?: boolean,
+}
+  

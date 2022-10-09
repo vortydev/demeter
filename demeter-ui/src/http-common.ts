d0a1dddda755e30adf87d7bcb6dbd71a1cd7c428
devweb <devweb.chronomed@gmail.com>
2022-10-08 21:41:06 -0400

    T

--- demeter-ui/src/http-common.ts
+++ demeter-ui/src/http-common.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || "http://127.0:8080/api",
   headers: {
-    "Content-type": "application/json"
+    "Content-Type": "application/json",
   }
+
 });

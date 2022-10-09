1876c8c29476fa4859720fb74c0524fa1b30336d
devweb <devweb.chronomed@gmail.com>
2022-10-09 09:16:16 -0400

    node_module

--- demeter-ui/src/App.tsx
+++ demeter-ui/src/App.tsx
@@ -1,45 +1,26 @@
-import React, { Component } from 'react';
-import { Switch, Route, Link } from "react-router-dom"
-import "bootstrap/dist/css/bootstrap.min.css";
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./static/css/App.css";
+import { getCookie } from "typescript-cookie";
+import { LoginPage } from "./components/Login Page/LoginPage";
+import { Navbar } from "./components/Templates/Navbar";
+import { Footerbar } from "./components/Templates/Footerbar";
+import { AccountPage } from "./components/AccountPage/AccountPage";
 
-import AddTutorial from "./components/add-tutorial.component";
-import Tutorial from "./components/tutorial.component";
-import TutorialsList from "./components/tutorials-list.component";
-
-class App extends Component {
-  render() {
+function App() {
+  const loggedIn = getCookie("account");
+  if (loggedIn == undefined) {
+    return <LoginPage />;
+  } else {
     return (
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/tutorials"} className="navbar-brand">
-            bezKoder
-          </Link>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/tutorials"} className="nav-link">
-                Tutorials
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
-                Add
-              </Link>
-            </li>
-          </div>
-        </nav>
-
-        <div className="container mt-3">
-          <Switch>
-            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
-            <Route exact path="/add" component={AddTutorial} />
-            <Route path="/tutorials/:id" component={Tutorial} />
-          </Switch>
-        </div>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+        </header>
+        <body className="App-body"><AccountPage/></body>
+        <Footerbar />
       </div>
     );
   }
-} 
+}
 
 export default App;
--- demeter-ui/src/components/AccountPage/EditPasswordForm.tsx
+++ demeter-ui/src/components/AccountPage/EditPasswordForm.tsx
@@ -1,4 +1,3 @@
-import { render } from "@testing-library/react";
 import { useState } from "react";
 import { Alert, Button, Form, Modal } from "react-bootstrap";
 

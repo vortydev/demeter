7ceb5503857d1820e50c7dcdfb0aae30753e347c
devweb <devweb.chronomed@gmail.com>
2022-10-11 11:58:58 -0400

    add button on RecipePage

--- demeter-ui/src/components/RecipePage/RecipePage.tsx
+++ demeter-ui/src/components/RecipePage/RecipePage.tsx
@@ -3,35 +3,75 @@ import { Alert, Button } from "react-bootstrap";
 import { CreateRecipeForm } from "./createRecipeForm";
 
 function RecipePage(): JSX.Element {
+  const [createRecipe, setCreateRecipe] = useState<boolean>(false);
+  const [createdSuccess, setSuccess] = useState<boolean>(false);
+  const [filter, setFilter] = useState<Number | null>(null);
 
-    const [createRecipe, setCreateRecipe] = useState<boolean>(false);
-    const [createdSuccess, setSuccess] = useState<boolean>(false);
-  
-    function success(): void {
-      setSuccess(true);
-      close();
-    }
-  
-    function close(): void {
-      setCreateRecipe(false);
-    }
-  
-    return (
-      <div>
-        {createdSuccess && <Alert>La recette à été créer avec succès!</Alert>}
-        <h1>News Page</h1>
-        <Button
-          variant="secondary"
-          onClick={() => {
-            setCreateRecipe(true);
-            setSuccess(false);
-          }}
-        >
-          Nouvelle Recette
-        </Button>
-        <CreateRecipeForm show={createRecipe} close={close} success={success} />
-      </div>
-    );
+  function success(): void {
+    setSuccess(true);
+    close();
+  }
+
+  function close(): void {
+    setCreateRecipe(false);
+  }
+
+  return (
+    <div>
+      {createdSuccess && <Alert>La recette à été créer avec succès!</Alert>}
+
+      <Button
+        onClick={() => {
+          setFilter(null);
+        }}
+        variant="secondary"
+      >
+        Tous
+      </Button>
+      <Button
+        onClick={() => {
+          setFilter(0);
+        }}
+        variant="secondary"
+      >
+        Boulangerie
+      </Button>
+      <Button
+        onClick={() => {
+          setFilter(1);
+        }}
+        variant="secondary"
+      >
+        Pâtisserie
+      </Button>
+      <Button
+        onClick={() => {
+          setFilter(2);
+        }}
+        variant="secondary"
+      >
+        Viennoiserie
+      </Button>
+      <Button
+        onClick={() => {
+          setFilter(3);
+        }}
+        variant="secondary"
+      >
+        Cuisine
+      </Button>
+      <Button
+        variant="dark"
+        onClick={() => {
+          setCreateRecipe(true);
+          setSuccess(false);
+        }}
+      >
+        Nouvelle Recette
+      </Button>
+      <CreateRecipeForm show={createRecipe} close={close} success={success} />
+    </div>
+  );
 }
 
-export {RecipePage}
+export { RecipePage };

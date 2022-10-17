1761d1a2b2087b3ba8377149228f44aad80207ad
DavidProg20 <dav20@live.ca>
2022-10-17 11:51:05 -0400

    news services and functions

---
+++ demeter-ui/src/services/news.functions.ts
@@ -0,0 +1,43 @@
+import AnnouncementsService from "./news.services";
+import { News} from "../types/Types";
+import bcrypt from "bcryptjs";
+
+async function createAnnouncement(data: News): Promise<boolean> {
+  const announcementCreated = AnnouncementsService.create(data)
+    .then((response: any) => {
+      return true;
+    })
+    .catch((e: Error) => {
+      console.log(e);
+      return false;
+    });
+  return announcementCreated;
+}
+
+async function updateAnnouncement(data: News, announcementName: String): Promise<boolean> {
+  const announcementUpdated = AnnouncementsService.update(data, announcementName)
+    .then((response: any) => {
+      return true;
+    })
+    .catch((e: Error) => {
+      console.log(e);
+      return false;
+    });
+  return announcementUpdated;
+}
+
+async function deleteAnnouncement(announcementName: string) {
+  const deleted = AnnouncementsService.delete(announcementName)
+    .then((response: any) => {
+      return true;
+    })
+    .catch((e: Error) => {
+      console.log(e);
+      return false;
+    });
+
+  return deleted;
+}
+
+
+export { createAnnouncement, updateAnnouncement,deleteAnnouncement };

296d4c39eed00a285934e7cba4d70700d440d828
IsabelleRioux <samoyede2003@gmail.com>
2022-10-17 10:07:18 -0400

    does nothing at the moment want to send online del

---
+++ demeter-ui/src/components/inventory/inventoryUpdateProduct.tsx
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+import { Form, Button, Modal } from 'react-bootstrap';
+import { Product } from '../../types/Types';
+import { GetCategory, GetMesurements, GetVendors } from './inventory';
+import { VendorForm } from './inventoryAddVendorForm';
+

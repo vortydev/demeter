a4df2e69cc1e3fa85a5d3cccea9dc59613b0067c
DavidProg20 <dav20@live.ca>
2022-10-19 11:24:03 -0400

    taskwork

--- demeter-ui/src/components/TaskPage/createTaskForm.tsx
+++ demeter-ui/src/components/TaskPage/createTaskForm.tsx
@@ -1,24 +1,44 @@
 import { render } from "@testing-library/react";
 import { useState } from "react";
 import { Alert, Button, Form, Modal } from "react-bootstrap";
+import { Task } from "../../types/Types";
 
 
 
 interface CRFormProps {
-  show :boolean;
+  show : boolean;
     close: () => void;
-    setSuccess: (succeed:boolean)=> void;
+    success: ()=> void;
   }
 
-  function CreateTaskForm({ show ,close, setSuccess }: CRFormProps) {
+  function CreateTaskForm({ show ,close, success }: CRFormProps) {
+
+    async function handlesubmit(){
+      const taskName = document.getElementById("taskName") as HTMLInputElement;
+      const description = document.getElementById("description") as HTMLInputElement;
+      const typeTask = document.getElementById("typeTask") as HTMLInputElement;
+      const taskParent = document.getElementById("") as HTMLInputElement;
+
+      const newTask: Task = {
+        taskName: taskName.value,
+        description: description.value,
+      };
+  
+      if (await createTask(newTask)) {
+        success();
+      } else {
+        setError(true);
+      }
+    }
+
     return(
       <Modal show={show} onHide={close}>
         <Form>
-          <Form.Group className="mb-3" controlId="task">
+          <Form.Group className="mb-3" controlId="taskName">
             <Form.Label>NOM : </Form.Label>
             <Form.Control type="text"/>
           </Form.Group>
-          <Form.Select className="mb-3" aria-label="TYPE : ">
+          <Form.Select className="mb-3" id="typeTask" aria-label="TYPE : ">
             <option></option>
             <option value="1">Quotidiennes</option>
             <option value="2">Hebdomadaires</option>
@@ -34,8 +54,8 @@ interface CRFormProps {
             <Form.Label>DESCRIPTION : </Form.Label>
             <Form.Control as="textarea" rows={3}/>
           </Form.Group>
-          <Button onClick={()=>{console.log("Click!")}}>Ajouter</Button>
-          <Button onClick={()=>{console.log("Cancel!")}}>Annuler</Button>
+          <Button onClick={handlesubmit}>Ajouter</Button>
+          <Button onClick={close}>Annuler</Button>
         </Form>
       </Modal>
     );

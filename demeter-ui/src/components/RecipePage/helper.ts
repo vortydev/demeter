d141b04cfd8930283e6d19cb8d935b5cdb47fe48
devweb <devweb.chronomed@gmail.com>
2022-10-19 16:28:23 -0400

    cleaning up cause we dont like dirty code

--- demeter-ui/src/components/RecipePage/helper.ts
+++ demeter-ui/src/components/RecipePage/helper.ts
@@ -1,49 +1,35 @@
-import { createImportSpecifier } from "typescript";
 import { IngForRecipe } from "../../types/RecipeTypes.types";
 import { Product } from "../../types/Types";
 
 export function pricePerQuantity(ingredient: IngForRecipe): number {
-  console.log("ingredient", ingredient);
-  console.log("mesurementID", ingredient.ingredient.mesurementId);
-
   let result: number = 0;
   if (ingredient.ingredient.mesurementId == ingredient.mesurementId) {
-    console.log("same unit");
-    // Si toute en gramme
-    result =
-      (parseFloat(ingredient.ingredient.price) /
-        parseFloat(ingredient.ingredient.qtyUnit)) *
-      ingredient.quantity;
-    console.log(result);
+    result = produitCroise(ingredient, ingredient.quantity);
   } else {
-    console.log("not same unit");
     let weightAdjustement = 0;
     switch (parseInt(ingredient.ingredient.mesurementId)) {
-      case 1 || 3: // Si produit en gramme ou mililitre
-        console.log(" in case 1");
+      case 1 || 3:
         weightAdjustement = ingredient.quantity * 1000;
-        result =
-          (parseFloat(ingredient.ingredient.price) /
-            parseFloat(ingredient.ingredient.qtyUnit)) *
-          weightAdjustement;
-
         break;
 
-      case 2 || 4: // si produit en kilogramme ou litre
-        console.log(" in case 2");
+      case 2 || 4:
         weightAdjustement = ingredient.quantity / 1000;
-
-        result =
-          (parseFloat(ingredient.ingredient.price) /
-            parseFloat(ingredient.ingredient.qtyUnit)) *
-          weightAdjustement;
         break;
 
       default:
-        console.log("in default");
+        //Do Nothing, this should not happen
         break;
     }
+    result = produitCroise(ingredient, weightAdjustement);
   }
 
   return Math.round((result + Number.EPSILON) * 100) / 100;
+}
+
+function produitCroise(ingredient: IngForRecipe, actualQty: number) {
+  return (
+    (parseFloat(ingredient.ingredient.price) /
+      parseFloat(ingredient.ingredient.qtyUnit)) *
+    actualQty
+  );
 }

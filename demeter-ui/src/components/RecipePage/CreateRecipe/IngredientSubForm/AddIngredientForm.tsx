549caa3c95bd16f6e1c6b0b009d3cd2839d82b49
devweb <devweb.chronomed@gmail.com>
2022-11-09 21:01:01 -0500

    ségrégation des type de mesure

--- demeter-ui/src/components/RecipePage/CreateRecipe/IngredientSubForm/AddIngredientForm.tsx
+++ demeter-ui/src/components/RecipePage/CreateRecipe/IngredientSubForm/AddIngredientForm.tsx
@@ -13,13 +13,10 @@ interface AIFProps {
   listIng: IngForRecipe[];
 }
 
-function AddIngredientForm({
-  show,
-  setShow,
-  listIng,
-}: AIFProps) {
+function AddIngredientForm({ show, setShow, listIng }: AIFProps) {
   const [productList, setProductList] = useState<Product[]>([]);
   const [mesureList, setListMesurement] = useState<Mesurement[]>([]);
+  const [specificMesure, setSpecificMesure] = useState<Mesurement[]>(mesureList);
   const [empty, setEmpty] = useState<boolean>(false);
 
   useEffect(() => {
@@ -30,10 +27,40 @@ function AddIngredientForm({
     getLists();
   }, [show]);
 
+  function updateMesurementOptions() {
+    const ingredient = (document.getElementById("product") as HTMLInputElement)
+      .value;
+
+    const productSelected: Product | undefined = productList.find(
+      (x) => x.id === parseInt(ingredient)
+    );
+
+    console.log(productSelected!.mesurementId);
+
+    const mesureId = parseInt(productSelected!.mesurementId);
+
+    if(mesureId === 1 || mesureId === 2 || mesureId === 6){
+      setSpecificMesure(
+        mesureList.filter(
+          (mesure) => mesure.id === 1 || mesure.id === 2 || mesure.id === 6
+        )
+      );
+
+    } else if(mesureId === 3 || mesureId == 4){
+      setSpecificMesure(
+        mesureList.filter((mesure) => mesure.id === 3 || mesure.id === 4)
+      );
+    } else {
+      setSpecificMesure(
+        mesureList.filter((mesure) => mesure.id === 5)
+      );
+    }
+
+  }
+
   const addToRecipeList = () => {
-    const ingredient = (
-      document.getElementById("product") as HTMLInputElement
-    ).value;
+    const ingredient = (document.getElementById("product") as HTMLInputElement)
+      .value;
     const quantity = parseInt(
       (document.getElementById("quantity") as HTMLInputElement).value
     );
@@ -60,25 +87,25 @@ function AddIngredientForm({
 
       listIng.push(toAdd);
       setShow(false);
-
-
-
     } else {
       setEmpty(true);
     }
     setTimeout(() => {
       setEmpty(false);
     }, 5000);
-  }
+  };
 
   return (
     <Modal show={show}>
       <Form className="popupForm">
         <h3 className="popupTitle">Ajouter un ingrédient</h3>
-        {empty && <Alert variant="danger">Veuillez remplir tous les champs.</Alert>}
+        {empty && (
+          <Alert variant="danger">Veuillez remplir tous les champs.</Alert>
+        )}
         <Form.Group className="popupSelectBox mb-2" controlId="product">
           <Form.Label className="popupSelectLabel">Produit</Form.Label>
-          <Form.Select id="product">
+          <Form.Select onChange={updateMesurementOptions} id="product">
+            <option>Choisir un ingrédient</option>
             {productList.map((product) => (
               <option value={product.id.toString()}>{product.name}</option>
             ))}
@@ -92,7 +119,7 @@ function AddIngredientForm({
           </Form.Group>
 
           <Form.Select id="mesurement">
-            {mesureList.map((mesure) => (
+            {specificMesure.map((mesure) => (
               <option value={mesure.id.toString()}>{mesure.mesurement}</option>
             ))}
           </Form.Select>
@@ -108,8 +135,12 @@ function AddIngredientForm({
         </div>
 
         <div className="popupBtnBox mt-3">
-          <Button variant="demeter-dark" onClick={() => setShow(false)}>Annuler</Button>
-          <Button variant="demeter" onClick={addToRecipeList}>Confirmer</Button>
+          <Button variant="demeter-dark" onClick={() => setShow(false)}>
+            Annuler
+          </Button>
+          <Button variant="demeter" onClick={addToRecipeList}>
+            Confirmer
+          </Button>
         </div>
       </Form>
     </Modal>
--- demeter-ui/src/components/RecipePage/helper.ts
+++ demeter-ui/src/components/RecipePage/helper.ts
@@ -1,10 +1,9 @@
 import { IngForRecipe } from "../../types/RecipeTypes.types";
 import { Product } from "../../types/Types";
 
-
-export function getRecipeCost(ingredients: IngForRecipe[]){
+export function getRecipeCost(ingredients: IngForRecipe[]) {
   let totalPrice = 0;
-  for(let ingredient of ingredients){
+  for (let ingredient of ingredients) {
     const price = pricePerQuantity(ingredient);
     totalPrice += price;
   }
@@ -14,23 +13,44 @@ export function getRecipeCost(ingredients: IngForRecipe[]){
 export function pricePerQuantity(ingredient: IngForRecipe): number {
   let result: number = 0;
 
-  if (ingredient.ingredient.mesurementId == ingredient.mesurementId) {
+  const qtyForRecipe = parseInt(ingredient.mesurementId);
+  const qtyOfProduct = parseInt(ingredient.ingredient.mesurementId);
+
+  if (qtyForRecipe === qtyOfProduct) {
     result = produitCroise(ingredient, ingredient.quantity);
   } else {
     let weightAdjustement = 0;
-    switch (parseInt(ingredient.ingredient.mesurementId)) {
-      case 1 || 3:
-        weightAdjustement = ingredient.quantity * 1000;
-        break;
-
-      case 2 || 4:
-        weightAdjustement = ingredient.quantity / 1000;
-        break;
-
-      default:
-        //Do Nothing, this should not happen
-        break;
+    if (
+      (qtyOfProduct === 1 || qtyOfProduct === 3) &&
+      (qtyForRecipe === 2 || qtyForRecipe === 4)
+    ) {
+     // ml or g to L or kg
+      weightAdjustement = ingredient.quantity * 1000;
+    } else if (
+      (qtyOfProduct === 2 || qtyOfProduct === 4) &&
+      (qtyForRecipe === 1 || qtyForRecipe === 3)
+    ) {
+      // kg or L to g or ml
+      weightAdjustement = ingredient.quantity / 1000;
+    } else if (qtyForRecipe === 6) {
+      if (qtyOfProduct === 1) {
+        // g to lb
+        weightAdjustement = ingredient.quantity * 1000 * 2.2;
+      } else if (qtyOfProduct === 2) {
+        // kg to lb
+        weightAdjustement = ingredient.quantity * 2.2;
+      }
+    } else if (qtyOfProduct === 6) {
+      if (qtyForRecipe === 1) {
+        // lb to g
+        weightAdjustement = ingredient.quantity / 1000 / 2.2;
+        
+      } else if (qtyForRecipe === 2) {
+        // lb to kg
+        weightAdjustement = ingredient.quantity / 2.2;
+      }
     }
+
     result = produitCroise(ingredient, weightAdjustement);
   }
 
@@ -44,5 +64,3 @@ function produitCroise(ingredient: IngForRecipe, actualQty: number) {
     actualQty
   );
 }
-
-

b348e410abf2c60f6802ad46d773f1062e66dd10
IsabelleRioux <samoyede2003@gmail.com>
2022-11-11 12:06:42 -0500

    migrated adding vendor to vendor display

--- demeter-ui/src/components/inventory/Vendor/vendorDisplay.tsx
+++ demeter-ui/src/components/inventory/Vendor/vendorDisplay.tsx
@@ -1,19 +1,36 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { useEffect, useState } from "react";
 import { Button, Container, Modal, ModalHeader, Row } from "react-bootstrap";
 import { getAllVendor } from "../../../services/vendor.functions";
 import { Vendor } from "../../../types/Types";
+import { VendorForm } from "./inventoryAddVendorForm";
 
 interface VendorDisplayProps {
     show: boolean;
     close: () => void;
 }
 function VendorDisplay({show, close}: VendorDisplayProps) {
-
+    const [createNewVendor, setCreateNewVendor] = useState<boolean>(false);
+    const [createdSuccess, setSuccess] = useState<boolean>(false);
     
+    function successVendor(): void {
+        setSuccess(true);
+        closeVendor();
+    }
+
+    function closeVendor(): void {
+        setCreateNewVendor(false);
+    }
+
     return (
         <Modal size="lg" show={show} onHide={close}>
             <h3 className="popupTitle">Liste des fournisseurs</h3>
+            <FontAwesomeIcon className="iconAdd iconEdit cursor" icon={faPlus} size="lg" onClick={() => {
+                setCreateNewVendor(true);
+                setSuccess(false);
+            }} />
             <Container>
                 <Row>
                     <h4>Nom</h4>
@@ -26,11 +43,11 @@ function VendorDisplay({show, close}: VendorDisplayProps) {
             <div className="mt-3 popupBtnBox">
                 <Button variant="demeter-dark" onClick={close}>Retour</Button>
             </div>
+            <VendorForm show={createNewVendor} close={closeVendor} success={successVendor} />
         </Modal>
     );
 }
 
-// insert code about displaying a vendor here
 function VendorList(){
     const [ vendors, setVendors ] = useState<Vendor[]>([]);
 
